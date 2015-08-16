b5f667a3ee274bb92bab7376c9f0f7431f5c7507
MorganW09 <morganwkenyon@gmail.com>
2015-08-16 18:13:20 -0500

    Updating spider to use CSV writer instead of just pringint to file.Cleaned up data files that didn't have any changes in them.

--- market/market/spiders/marketspider.py
+++ market/market/spiders/marketspider.py
@@ -15,6 +15,9 @@ class MarketSpider(Spider):
     allowed_domains = ["http://www.nasdaq.com/"]
     data_folder = str(expanduser("~")) + "/code/WebScraper/data/"
     data_file = "stockprices.csv"
+    csvHeader = ["Symbol", "Price", "Date", "Volume"]
+    dataList = []
+    writtenHeader = False
 
     def __init__(self, name=None, **kwargs):
         if name is not None:
@@ -25,7 +28,6 @@ class MarketSpider(Spider):
         if not hasattr(self, 'start_urls'):
             self.start_urls = []
         self.getUrls()
-        #today = datetime.date.today()
         utc = str(datetime.utcnow())
         utc = utc.split(".")[0].replace("-", ".").replace(" ", ".").replace(":",".")
         self.data_file = utc + self.data_file
@@ -37,45 +39,59 @@ class MarketSpider(Spider):
         price = hxs.xpath('//*[@id="qwidget_lastsale"]/text()').extract()
         stock = hxs.xpath('//*[@id="left-quotes-content"]/div[1]/span/a[3]/text()').extract()
         volume = hxs.xpath('//*[@id="quotes_content_left__Volume"]/text()').extract()
+        writerList = []
+
         #stock
         stock = stock[0].encode('ascii','ignore')
+        writerList.append(stock)
+
         #price
         if(len(price) > 0):
             price = price[0].encode('ascii','ignore')
         else:
             price = "NULL"
+        writerList.append(price.replace("$", ""))
+        #date
+        millis = int(round(time.time() * 1000))
+        writerList.append(millis)
+
         #volume
         if(len(volume) > 0):
             volume = volume[0].encode('ascii','ignore').replace(",","")
         else:
             volume = 0
-        self.appendStock(stock, price, volume)
+        writerList.append(volume)
+
+        self.appendCSV(writerList)
         #spider stuff
         item = MarketSpider()
         yield item
 
     def start_requests(self):
         utc = datetime.utcnow()
-        beforeMarket = utc.hour <= 21 and utc.minute <= 20
-        afterMarket = utc.hour >= 14 and utc.minute >= 10
-        marketDay = utc.weekday() < 6 #0 is Monday, 6 is Sunday
+        beforeMarketCloses = utc.hour <= 21 and utc.minute <= 20
+        afterMarketOpens = utc.hour >= 14 and utc.minute >= 10
+        isMarketDay = utc.weekday() < 6 #0 is Monday, 6 is Sunday
         if(True):
-        #if(marketDay and beforeMarket and afterMarket):
+        #if(isMarketDay and beforeMarketCloses and afterMarketOpens):
             for url in self.start_urls:
                 yield self.make_requests_from_url(url)
 
     def getUrls(self):
-        print ("from getUrls()")
-        #self.start_urls = ["http://www.nasdaq.com/symbol/aapl/real-time"]
         with open(self.data_folder + 'companylist.csv', 'rb') as csvfile:
             csvreader = csv.reader(csvfile, delimiter=',', quotechar='"')
             for row in csvreader:
-                self.concatUrls(row[0])
+                if row[0] != "Symbol":
+                    self.concatUrls(row[0].replace(" ",""))
 
     def concatUrls(self, symbol):
         self.start_urls.append("http://www.nasdaq.com/symbol/" + symbol + "/real-time")
 
-    def appendStock(self, stock, price, volume):
-        millis = int(round(time.time() * 1000))
-        with open(self.data_folder + self.data_file, "a") as myfile:
-            myfile.write(stock + "," + price + "," + str(millis) + "," + str(volume) + "\n")
+
+    def appendCSV(self, writeList):
+        with open (self.data_folder + self.data_file, "a") as csvfile:
+            stockwriter = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            if(not self.writtenHeader):
+                stockwriter.writerow(self.csvHeader)
+                self.writtenHeader = True
+            stockwriter.writerow(writeList)
